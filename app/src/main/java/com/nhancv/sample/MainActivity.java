cb82ba67f516c91454c377e67c4ecf8029aec8d6
Nhan Cao <nhancv1992@gmail.com>
2016-11-15 14:12:16 +0700

    update library

--- app/src/main/java/com/nhancv/sample/MainActivity.java
+++ app/src/main/java/com/nhancv/sample/MainActivity.java
@@ -1,13 +1,20 @@
 package com.nhancv.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+
+import com.nhancv.nexpandable.ExpandableLayoutListView;
 
 public class MainActivity extends AppCompatActivity {
+    private final String[] array = {"Hello", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome", "World", "Android", "is", "Awesome"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.view_row, R.id.header_text, array);
+        final ExpandableLayoutListView expandableLayoutListView = (ExpandableLayoutListView) findViewById(R.id.listview);
+        expandableLayoutListView.setAdapter(arrayAdapter);
     }
 }
